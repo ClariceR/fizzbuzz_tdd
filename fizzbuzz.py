65a57571e557aea7d5439b098b014aa767a0c273
cla <claricerdallorto@gmail.com>
2021-01-16 15:02:06 +0000

    says fizz, buzz and fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,17 @@
 
 
-def is_divisible_by_three(number):
-    return number % 3 == 0
+def is_divisible_by(number, divisor):
+    return number % divisor == 0
 
 
-def is_divisible_by_five(number):
-    return number % 5 == 0
+def say(number):
+    if is_divisible_by(number, 3) and is_divisible_by(number, 5):
+        return 'fizzbuzz'
+    elif is_divisible_by(number, 3):
+        return 'fizz'
+    elif is_divisible_by(number, 5):
+        return 'buzz'
+    else:
+        return number
 
 
-def is_divisible_by_three_and_five(number):
-    return number % 3 == 0 and number % 5 == 0
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -3,25 +3,32 @@ import fizzbuzz
 
 
 def test_number_is_divisible_by_three():
-    assert fizzbuzz.is_divisible_by_three(3) == True
+    assert fizzbuzz.is_divisible_by(3, 3)
 
 
 def test_number_is_NOT_divisible_by_three():
-    assert fizzbuzz.is_divisible_by_three(1) == False
+    assert not fizzbuzz.is_divisible_by(1, 3)
 
 
 def test_number_is_divisible_by_five():
-    assert fizzbuzz.is_divisible_by_five(5) == True
+    assert fizzbuzz.is_divisible_by(5, 5)
 
 
 def test_number_is_NOT_divisible_by_five():
-    assert fizzbuzz.is_divisible_by_five(1) == False
+    assert not fizzbuzz.is_divisible_by(1, 5)
 
 
-def test_number_is_divisible_by_both_three_and_five():
-    assert fizzbuzz.is_divisible_by_three_and_five(15) == True
+def test_say_fizz():
+    assert fizzbuzz.say(3) == 'fizz'
 
 
-def test_number_is_NOT_divisible_by_both_three_and_five():
-    assert fizzbuzz.is_divisible_by_three_and_five(1) == False
-    
+def test_say_buzz():
+    assert fizzbuzz.say(5) == 'buzz'
+
+
+def test_say_fizzbuzz():
+    assert fizzbuzz.say(15) == 'fizzbuzz'
+
+
+def test_return_number():
+    assert fizzbuzz.say(11) == 11
