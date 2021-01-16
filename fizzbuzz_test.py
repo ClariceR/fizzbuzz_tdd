02de1ab254a4ba493b436fe868514cbec50d80aa
cla <claricerdallorto@gmail.com>
2021-01-16 14:28:33 +0000

    knows if numbers are divisible by 3, 5 or 3 and 5

---
+++ fizzbuzz_test.py
@@ -0,0 +1,27 @@
+import pytest
+import fizzbuzz
+
+
+def test_number_is_divisible_by_three():
+    assert fizzbuzz.is_divisible_by_three(3) == True
+
+
+def test_number_is_NOT_divisible_by_three():
+    assert fizzbuzz.is_divisible_by_three(1) == False
+
+
+def test_number_is_divisible_by_five():
+    assert fizzbuzz.is_divisible_by_five(5) == True
+
+
+def test_number_is_NOT_divisible_by_five():
+    assert fizzbuzz.is_divisible_by_five(1) == False
+
+
+def test_number_is_divisible_by_both_three_and_five():
+    assert fizzbuzz.is_divisible_by_three_and_five(15) == True
+
+
+def test_number_is_NOT_divisible_by_both_three_and_five():
+    assert fizzbuzz.is_divisible_by_three_and_five(1) == False
+    
